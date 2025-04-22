2ae78efa060a169af46d54a804a11734ea592910
jyayin <jyayinn@gmail.com>
2025-04-22 21:05:20 +0800

    all data regenerated with updated synthesizer, ground truth added too

--- jya/config_hyperparameters.py
+++ jya/config_hyperparameters.py
@@ -14,47 +14,47 @@ STRATEGIES = {
         "od_minority": IQRDetector(factor=1),
         "scaler": "Robust"
     },
-    "DATA_2": {
+    "DATA_1_2": {
         "synthesizer": "TVAE",
         "epochs": 1000,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1.5),
         "od_minority": IQRDetector(factor=1.5),
-        "scaler": "Robust"
+        "scaler": "Standard"
     },
-    "DATA_3": {
+    "DATA_1_3": {
         "synthesizer": "TVAE",
         "epochs": 2500,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1.5),
         "od_minority": IQRDetector(factor=1.5),
-        "scaler": "Robust"
+        "scaler": "Standard"
     },
-    "DATA_4": {
+    "DATA_1_4": {
         "synthesizer": "TVAE",
         "epochs": 1000,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1.5),
         "od_minority": IQRDetector(factor=1.5),
-        "scaler": "Robust"
+        "scaler": "Standard"
     },
-    "DATA_5": {
+    "DATA_1_5": {
         "synthesizer": "CTGAN",
         "epochs": 1000,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1.5),
         "od_minority": IQRDetector(factor=1.5),
-        "scaler": "Robust"
+        "scaler": "Standard"
     },
-    "DATA_6": {
+    "DATA_1_6": {
         "synthesizer": "CTGAN",
         "epochs": 2500,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1.5),
         "od_minority": IQRDetector(factor=1.5),
-        "scaler": "Robust"
+        "scaler": "Standard"
     },
-    "DATA_7": {
+    "DATA_1_7": {
         "synthesizer": "CTGAN",
         "epochs": 2500,
         "n_synthetic": 10000,
@@ -62,12 +62,28 @@ STRATEGIES = {
         "od_minority": IQRDetector(factor=1),
         "scaler": "Robust"
     },
-    "DATA_8": {
+    "DATA_1_8": {
         "synthesizer": "CTGAN",
         "epochs": 1000,
         "n_synthetic": 10000,
         "od_majority": IQRDetector(factor=1),
         "od_minority": IQRDetector(factor=1),
         "scaler": "Robust"
+    },
+    "DATA_9": {
+        "synthesizer": "TVAE",
+        "epochs": 1000,
+        "n_synthetic": 10000,
+        "od_majority": IQRDetector(factor=1.2),
+        "od_minority": IQRDetector(factor=1.8),
+        "scaler": "Standard"
+    },
+    "DATA_10": {
+        "synthesizer": "CTGAN",
+        "epochs": 1000,
+        "n_synthetic": 10000,
+        "od_majority": IQRDetector(factor=1),
+        "od_minority": IQRDetector(factor=1.5),
+        "scaler": "Standard"
     }
 }
